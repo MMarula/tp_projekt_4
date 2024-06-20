7113f79c9086df368095787f310aec784515d61f
Antonina Wlodarczyk <antoninawlodarczyk55@gmail.com>
2024-06-20 20:14:32 +0200

    wyglad drona

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -13,10 +13,30 @@ void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer)
     float q_x, q_y, q_theta;
 
     /* x, y, theta coordinates */
-    q_x = state[0];
-    q_y = state[1];
+    q_x = 640 + state[0];
+    q_y = 360 + state[1];
     q_theta = state[2];
 
     SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0x00, 0x00, 0xFF);
     filledCircleColor(gRenderer.get(), q_x, q_y, 30, 0xFF0000FF);
+
+    float R = 10.0f;
+    filledCircleColor(gRenderer.get(), q_x + 50, q_y, R, 0x6082B6FF);
+    filledCircleColor(gRenderer.get(), q_x - 50, q_y, R, 0x6082B6FF);
+
+    SDL_SetRenderDrawColor(gRenderer.get(), 0x60, 0x82, 0xB6, 0xFF);
+    SDL_RenderDrawLine(gRenderer.get(), q_x, q_y, q_x + 50, q_y);
+    SDL_RenderDrawLine(gRenderer.get(), q_x, q_y, q_x - 50, q_y);
+
+    static float angle = 0.0f;
+    angle += 0.1f;
+
+    float R_x1 = q_x + 50 + R * cos(angle);
+    float R_y1 = q_y + R * sin(angle);
+    float R_x2 = q_x - 50 + R * cos(angle);
+    float R_y2 = q_y + R * sin(angle);
+
+    SDL_SetRenderDrawColor(gRenderer.get(), 0x60, 0x82, 0xB6, 0xFF);
+    SDL_RenderDrawLine(gRenderer.get(), q_x + 50, q_y, R_x1, R_y1);
+    SDL_RenderDrawLine(gRenderer.get(), q_x - 50, q_y, R_x2, R_y2);
 }

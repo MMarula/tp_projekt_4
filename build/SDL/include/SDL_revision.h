57074916e35911fc8cbd965332a8d5471a4b08eb
Antonina Wlodarczyk <antoninawlodarczyk55@gmail.com>
2024-06-21 00:43:54 +0200

    poprawa lotu v1

--- build/SDL/include/SDL_revision.h
+++ build/SDL/include/SDL_revision.h
@@ -1,4 +1,4 @@
-#define SDL_REVISION "https://github.com/marekjg/tp_projekt_4@7113f79c9086df368095787f310aec784515d61f"
+#define SDL_REVISION "https://github.com/marekjg/tp_projekt_4@ccb3a12e9ef784ade385d121c72f5b9941b544aa"
 #define SDL_REVISION_NUMBER 0
 
 #ifndef SDL_REVISION
